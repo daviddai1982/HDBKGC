beaeebc3fc0692cab78d06eb9ccd3e18913e0577
DavidDai <dai.david2005@gmail.com>
2014-02-13 21:52:04 +0800

    Update TableCell Stroyboard

--- HDBKGC/HDBKGC/HD/IndexVP/VPTableContentViewController.m
+++ HDBKGC/HDBKGC/HD/IndexVP/VPTableContentViewController.m
@@ -113,7 +113,7 @@
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell * cell = [self tableView:tableView cellForRowAtIndexPath:indexPath];
-    return cell.frame.size.height;
+    return cell.frame.size.height+1;
 }
 
 @end
